cfd8e0241917d77f1c51937782214272d1a2b557
JhayRivera <jhayjane78@gmail.com>
2025-12-16 10:52:59 +0800

    fixing deployment issue

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python src/manage.py collectstatic --no-input
 
-python manage.py migrate
+python src/manage.py migrate
